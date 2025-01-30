6459e9bdc9b5d20818e3a4bc62a62c15f31e0f24
DiogoCass <100029911+DiogoCass@users.noreply.github.com>
2025-01-29 22:10:10 -0300

    Create start.js

---
+++ subcodes/start.js
@@ -0,0 +1,3 @@
+"use strict";
+import detalhes from 'https://raw.githubusercontent.com/DiogoCass/componentes/refs/heads/main/subcodes/pequenosdetalhes.json' assert {type: 'json'};
+console.log(detalhes)
